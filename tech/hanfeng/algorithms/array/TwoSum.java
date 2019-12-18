1eb261bf676cfd6b0a39eb8baf28026924658b06
Feng Ping Liu <liufengping@hanfeng.tech>
2019-12-19 02:12:39 +0800

    added problem description and output

--- tech/hanfeng/algorithms/array/TwoSum.java
+++ tech/hanfeng/algorithms/array/TwoSum.java
@@ -4,10 +4,25 @@ import java.util.Map;
 import java.util.HashMap;
 
 /**
+ * @problem
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
  * 
- * @easy
- * @author andy
+ * @example
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * 
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * 
+ * return [0, 1].
+ * 
+ * @time_complexity 
+ * O(N)
  *
+ * @space_comlexity
+ * O(N)
+ *
+ * @difficulty
+ * Easy
  */
 public class TwoSum {
 
--- tech/hanfeng/algorithms/array/client/TwoSumClient.java
+++ tech/hanfeng/algorithms/array/client/TwoSumClient.java
@@ -5,12 +5,19 @@ import tech.hanfeng.algorithms.array.TwoSum;
 
 public class TwoSumClient {
 
+	/**
+	 * @output
+	 * 
+	 *  [0, 1]
+	 *  Exception in thread "main" java.lang.IllegalArgumentException: No two sum solution
+	 *          at tech.hanfeng.algorithms.array.TwoSum.twoSum(TwoSum.java:40)
+	 *          at tech.hanfeng.algorithms.array.client.TwoSumClient.main(TwoSumClient.java:21)
+	 */
 	public static void main(String[] args) {
-		int[] nums = {1, 5, 8, 9};
-		System.out.println(Arrays.toString(new TwoSum().twoSum(nums, 10)));
+		int[] nums = {2, 7, 11, 15};
+		System.out.println(Arrays.toString(new TwoSum().twoSum(nums, 9)));
 		
-		int[] nums2 = {1, 5, 8, 9};
+		int[] nums2 = {2, 7, 11, 15};
 		System.out.println(Arrays.toString(new TwoSum().twoSum(nums2, 16)));
-		
 	}
 }
