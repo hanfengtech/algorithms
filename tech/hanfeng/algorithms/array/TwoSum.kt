097dcc863775f305dec6426524bf5b72bd85196b
Feng Ping Liu <liufengping@hanfeng.tech>
2020-02-24 16:56:26 -0800

    used more advance kotlin syntax and library function

--- tech/hanfeng/algorithms/array/TwoSum.kt
+++ tech/hanfeng/algorithms/array/TwoSum.kt
@@ -2,32 +2,35 @@ package tech.hanfeng.algorithms.array
 import kotlin.collections.HashMap
 
 /**
-* @example
-* Given nums = [2, 7, 11, 15], target = 9,
-*
-* Because nums[0] + nums[1] = 2 + 7 = 9,
-*
-* return [0, 1].
-*
-* @time_complexity
-* O(N)
-*
-* @space_comlexity
-* O(N)
-*
-* @difficulty
-* Easy
-*/
+ *
+ * Given an array of integers and see if there is any two integers add up to a target value.  Returns the index of the
+ * two integers
+ *
+ * @example
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ *
+ * return [0, 1].
+ *
+ * @time_complexity
+ * O(N)
+ *
+ * @space_comlexity
+ * O(N)
+ *
+ * @difficulty
+ * Easy
+ */
 fun twoSum(nums : IntArray, target: Int) : IntArray {
     val map = HashMap<Int, Int>()
 
-    var i = 0
-    for (num in nums) {
+    for ((i, num) in nums.withIndex()) {
         val complement = target - num
         if (map.containsKey(complement)) {
             return intArrayOf(map[complement]!!, i)
         } else {
-            map[num] = i++
+            map[num] = i
         }
     }
     throw IllegalArgumentException("No two sum solution")
@@ -43,6 +46,6 @@ fun twoSum(nums : IntArray, target: Int) : IntArray {
  *          at tech.hanfeng.algorithms.array.client.TwoSumClient.main(TwoSumClient.java:21)
  */
 fun main() {
-    println(twoSum(intArrayOf(2, 7, 11, 15), 9).contentToString())
+    println(twoSum(intArrayOf(2, 7, 11, 15), 13).contentToString())
     println(twoSum(intArrayOf(2, 7, 11, 15), 30).contentToString())
 }
