2b836b0ff13c2d1f1118e1fed069eede34085092
Feng Ping Liu <liufengping@hanfeng.tech>
2020-02-24 16:33:05 -0800

    Remove duplicates in ordered array in place and return the new length.

---
+++ tech/hanfeng/algorithms/array/RemoveDuplicates.kt
@@ -0,0 +1,37 @@
+package tech.hanfeng.algorithms.array
+
+/**
+ * Remove duplicates in a sorted array.
+ *
+ * T(O) = O(N)
+ *
+ * S(O) = O(1)
+ *
+ * Difficulty: Easy
+ *
+ * @input [1,1,2]
+ */
+fun removeDuplicates(nums : IntArray) : Int {
+    if (nums.isEmpty()) return 0
+
+    var i = 0
+    for (j in 1 until nums.size) {
+        if (nums[i] != nums[j]) {
+            nums[++i] = nums[j]
+        }
+    }
+
+    return ++i
+}
+
+/**
+ * @output
+ *
+ * 2
+ * [1, 2, 2]
+ */
+fun main() {
+    var nums = intArrayOf(1,1,2)
+    println(removeDuplicates(nums))
+    println(nums.contentToString())
+}
