b114c929b82112492714a3ae9e31200ba40b8149
Feng Ping Liu <liufengping@hanfeng.tech>
2020-02-28 16:14:19 -0800

    calculate the max path sum

---
+++ tech/hanfeng/algorithms/tree/MaxPathSum.kt
@@ -0,0 +1,27 @@
+package tech.hanfeng.algorithms.tree
+
+import tech.hanfeng.algorithms.tree.utils.TreeNode
+import kotlin.math.max
+
+fun maxPathSum(root : TreeNode?) : Int {
+    var maxValue = Int.MIN_VALUE
+
+    fun helper(node : TreeNode?) : Int {
+        if (node == null) return 0
+
+        val left = max(0, helper(node.left))
+        val right = max(0, helper(node.right))
+        maxValue = max(maxValue, left + right + node.value)
+        return max(left, right) + node.value
+    }
+
+    helper(root)
+    return maxValue
+}
+
+fun main() {
+    val tree = TreeNode(1)
+    tree.left = TreeNode(2)
+    tree.right = TreeNode(3)
+    print(maxPathSum(tree))
+}
