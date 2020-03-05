a562499520c948bc15e0eb0bfc77d616c4a3eee1
Feng Ping Liu <liufengping@hanfeng.tech>
2020-03-05 08:59:18 -0800

    Letter case permutation

---
+++ tech/hanfeng/algorithms/recursion/LetterCasePermutation.kt
@@ -0,0 +1,42 @@
+package tech.hanfeng.algorithms.recursion
+
+import java.util.*
+import kotlin.collections.ArrayList
+
+fun letterPermutation(S: String) : List<String> {
+    val resList = ArrayList<String> ()
+
+    fun helper(slate : StringBuilder, i : Int) {
+        if (i == S.length) {
+            resList.add(slate.toString())
+        } else {
+            if (S[i].isDigit()) {
+                slate.append(S[i])
+                helper(slate, i + 1)
+                slate.removeLast()
+                var temp = 0
+                temp++
+            } else {
+                slate.append(S[i].toLowerCase())
+                helper(slate, i + 1)
+                slate.removeLast()
+
+                slate.append(S[i].toUpperCase())
+                helper(slate, i + 1)
+                slate.removeLast()
+            }
+        }
+    }
+
+    helper(StringBuilder(), 0)
+    return resList
+}
+
+fun StringBuilder.removeLast() {
+    deleteCharAt(lastIndex)
+}
+
+fun main() {
+    println(letterPermutation("a1b2"))
+}
+
