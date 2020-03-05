81459e70c90f8deb16a738198902ee50e5135e0e
Feng Ping Liu <liufengping@hanfeng.tech>
2020-03-05 12:25:25 -0800

    letter combinations

--- tech/hanfeng/algorithms/recursion/DecimalString.kt
+++ tech/hanfeng/algorithms/recursion/DecimalString.kt
@@ -8,10 +8,11 @@ fun decimalString(n : Int) {
     fun helper(slate : String, n : Int) {
         if (n == 0) {
             println(slate)
-        } else {
-            for (i in 0 .. 9) {
-                helper(slate + i, n -1)
-            }
+            return
+        }
+
+        for (i in 0..9) {
+            helper(slate + i, n - 1)
         }
     }
 
--- tech/hanfeng/algorithms/recursion/LetterCombinations.kt
+++ tech/hanfeng/algorithms/recursion/LetterCombinations.kt
@@ -1,2 +1,40 @@
 package tech.hanfeng.algorithms.recursion
 
+fun letterCombination(digits : String) : List<String> {
+    val resList = ArrayList<String>()
+    if (digits.isEmpty()) return resList
+
+    val map = mapOf('2' to arrayOf("a", "b", "c"),
+            '3' to arrayOf("d", "e", "f"),
+            '4' to arrayOf("g", "h", "i"),
+            '5' to arrayOf("j", "k", "l"),
+            '6' to arrayOf("m", "n", "o"),
+            '7' to arrayOf("p", "q", "s", "r"),
+            '8' to arrayOf("t", "u", "v"),
+            '9' to arrayOf("w", "x", "y", "z")
+    )
+
+    fun helper(slate : StringBuilder, i : Int) {
+        if (i == digits.length) {
+            if (slate.isNotEmpty()) {
+                resList.add(slate.toString())
+                return
+            }
+        }
+        val letters = map[digits[i]]
+        if (letters != null) {
+            for (letter in letters) {
+                slate.append(letter)
+                helper(slate, i + 1)
+                slate.deleteCharAt(slate.lastIndex)
+            }
+        }
+    }
+
+    helper(StringBuilder(), 0)
+    return resList
+}
+
+fun main() {
+    println(letterCombination("23"))
+}
