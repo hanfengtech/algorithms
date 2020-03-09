d89723e56ccd87d3bea5454226a4488f5322950a
Feng Ping Liu <liufengping@hanfeng.tech>
2020-03-09 12:07:15 -0700

    sqrt a number

---
+++ tech/hanfeng/algorithms/math/Sqrt.kt
@@ -0,0 +1,21 @@
+package tech.hanfeng.algorithms.math
+
+fun sqrt(x: Int): Int {
+    var lo = 0L
+    var hi = x.toLong()
+    while (hi >= lo) {
+        var mid = lo + (hi - lo) / 2
+        if (x < mid * mid) {
+            hi = mid - 1
+        } else{
+            lo = mid + 1
+        }
+    }
+
+    return hi.toInt()
+}
+
+fun main() {
+    println(sqrt(4))
+    println(sqrt(2147395599))
+}
