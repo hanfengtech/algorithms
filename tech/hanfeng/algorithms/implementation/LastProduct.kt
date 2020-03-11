64b35a558515f010b616a3a76ba83e716e19fe08
Feng Ping Liu <liufengping@hanfeng.tech>
2020-03-11 00:43:23 -0700

    added overflow check

--- tech/hanfeng/algorithms/implementation/LastProduct.kt
+++ tech/hanfeng/algorithms/implementation/LastProduct.kt
@@ -1,5 +1,6 @@
 package tech.hanfeng.algorithms.implementation
 
+import java.lang.ArithmeticException
 import java.util.*
 
 /**
@@ -12,50 +13,63 @@ import java.util.*
  */
 
 
-class LastProduct{
+class LastProduct {
     var k: Int
     var values = LinkedList<Int>()
     var zeros = 0
-    var product : Int = 1
+    var max = 0
+    var product: Int = 1
 
-    constructor(k : Int) {
+    constructor(k: Int) {
         this.k = k
     }
 
-    fun add(x : Int) {              // O(1)
+    fun add(x: Int) {              // O(1)
         if (values.size == k) {     // size reached k
             val dump = values.poll()
             if (zeros > 0) {  // there are zeros
                 zeros--
             } else {  // there are no zeros
-                product /= dump
+                if (max > 0) {
+                    max--
+                } else {
+                    product /= dump
+                }
             }
         }
 
         values.offer(x)
-        if (x == 0) {
+        if (x == 0) {   // has zeros
             zeros = values.size
             product = 1
-        } else {
-            product *= x
+            max = 0
+        } else {        // no zeros
+            product = try {
+                Math.multiplyExact(product, x)
+            } catch (ae: ArithmeticException) {
+                max = values.size
+                1
+            }
         }
     }
 
-    fun get() : Int {                //  O(1)
-        return if (values.size != 0) {
-            if (zeros > 0) {
-                0
-            } else {
-                product
-            }
-        } else {
-            0
-        }
+    fun get(): Int {                //  O(1)
+        if (values.size == 0)
+            return 0
+
+        if (zeros > 0)
+            return 0
+
+        if (max > 0)
+            return Int.MAX_VALUE
+
+        return product
     }
 }
 
 fun main() {
     var lp = LastProduct(3)
+
     println(lp.get())
 
     lp.add(1)
@@ -76,9 +90,31 @@ fun main() {
     lp.add(2)
     println(lp.get())
 
+    lp.add(Int.MAX_VALUE)
+    println(lp.get())
+
     lp.add(2)
+    println(lp.get())
+
+    lp.add(3)
+    println(lp.get())
+
+    lp.add(2)
+    println(lp.get())
+
+    lp.add(Int.MAX_VALUE)
     println(lp.get())
 
     lp.add(0)
     println(lp.get())
+
+    lp.add(4)
+    println(lp.get())
+
+    lp.add(5)
+    println(lp.get())
+
+    lp.add(5)
+    println(lp.get())
+
 }
