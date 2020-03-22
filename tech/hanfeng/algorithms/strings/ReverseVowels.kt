6c247cf237aaff9c739bde5c7659b5f610c05415
Feng Ping Liu <liufengping@hanfeng.tech>
2020-03-22 08:00:01 -0700

    Reverse vowels

--- tech/hanfeng/algorithms/strings/ReverseVowels.kt
+++ tech/hanfeng/algorithms/strings/ReverseVowels.kt
@@ -1,2 +1,23 @@
 package tech.hanfeng.algorithms.strings
 
+fun reverseVowels(s: String): String {
+
+    var a = s.toCharArray()
+
+    var i = 0
+    var j = s.length - 1
+
+    val vowels = HashSet(arrayListOf('a','e','i','o','u'))
+
+    while (i < j) {
+        while (i < j && !vowels.contains(a[i].toLowerCase())) i++
+        while (j > i && !vowels.contains(a[j].toLowerCase())) j--
+        swap(a, i++, j--)
+    }
+
+    return String(a)
+}
+
+fun main() {
+    println(reverseVowels("leetcode"))
+}
