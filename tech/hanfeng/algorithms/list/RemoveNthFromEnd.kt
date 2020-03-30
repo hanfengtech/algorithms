9aaa7e2ff81c63cb4c3366e8e23ced9bef496124
Feng Ping Liu <liufengping@hanfeng.tech>
2020-03-30 13:13:27 -0700

    handle corner cases

--- tech/hanfeng/algorithms/list/RemoveNthFromEnd.kt
+++ tech/hanfeng/algorithms/list/RemoveNthFromEnd.kt
@@ -1,2 +1,45 @@
 package tech.hanfeng.algorithms.list
 
+import tech.hanfeng.algorithms.list.utils.ListNode
+import tech.hanfeng.algorithms.list.utils.printList
+
+fun removeNthFromEnd(head : ListNode?, n : Int)  : ListNode? {
+
+    if (n < 1 || head == null) {
+        return head
+    }
+
+    val start : ListNode ?= ListNode(0)
+    start!!.next = head
+
+    var fast = start
+    var slow = start
+
+    var i = 0
+    while (i++ <= n && fast != null) {
+        fast = fast.next
+    }
+
+    if (i != n + 2) {
+        return head
+    }
+
+    while(fast != null) {
+        fast = fast.next
+        slow = slow!!.next
+    }
+
+    slow!!.next = slow.next!!.next
+
+    return start.next
+}
+
+fun main() {
+    val node = ListNode(1)
+    node.next = ListNode(2)
+    node.next!!.next = ListNode(3)
+    node.next!!.next!!.next = ListNode(4)
+    node.next!!.next!!.next!!.next = ListNode(5)
+
+    printList(removeNthFromEnd(node, 2))
+}
--- tech/hanfeng/algorithms/list/utils/Utils.kt
+++ tech/hanfeng/algorithms/list/utils/Utils.kt
@@ -1,12 +1,15 @@
 package tech.hanfeng.algorithms.list.utils
 
-fun printList(node : ListNode) {
+fun printList(node : ListNode?) {
+    if (node == null)
+        return
+
     var cur = node
     if (cur.next !== null) {
-        while (cur.next != null) {
-            print("${cur.value} -> ")
-            cur = cur.next!!
+        while (cur?.next != null) {
+            print("${cur?.value} -> ")
+            cur = cur?.next
         }
     }
-    print(cur.value)
+    print(cur?.value)
 }
