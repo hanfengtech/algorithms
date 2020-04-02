3a7cc8a52c8d9521aeb96354019d11fc54383789
Feng Ping Liu <liufengping@hanfeng.tech>
2020-04-01 17:10:06 -0700

    use reduce

--- tech/hanfeng/algorithms/array/SingleNumber.kt
+++ tech/hanfeng/algorithms/array/SingleNumber.kt
@@ -1,17 +1,27 @@
 package tech.hanfeng.algorithms.array
 
 fun singleNumber(nums: IntArray) : Int {
+    if (nums.size == 1) {
+        return nums[0]
+    }
+
     var result = 0
 
-    for(num in nums) {
-        result = result.xor(num)
+    nums.forEach{
+        result = result xor it
     }
 
     return result
 }
 
+fun singleNumberOneLine(nums: IntArray) : Int = nums.reduce { a, b -> a xor b }
+
 fun main() {
     println(singleNumber(intArrayOf(2,2,1)))
     println(singleNumber(intArrayOf(4,1,2,1,2)))
     println(singleNumber(intArrayOf(6,2,2,6,7)))
+
+    println(singleNumberOneLine(intArrayOf(2,2,1)))
+    println(singleNumberOneLine(intArrayOf(4,1,2,1,2)))
+    println(singleNumberOneLine(intArrayOf(6,2,2,6,7)))
 }
