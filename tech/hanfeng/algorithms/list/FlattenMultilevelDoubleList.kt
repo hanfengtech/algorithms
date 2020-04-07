51b27cb187b74ae609f64636af84140689337ff4
Feng Ping Liu <liufengping@hanfeng.tech>
2020-04-07 11:56:30 -0700

    refactored

--- tech/hanfeng/algorithms/list/FlattenMultilevelDoubleList.kt
+++ tech/hanfeng/algorithms/list/FlattenMultilevelDoubleList.kt
@@ -21,35 +21,41 @@ fun printList(node : Node?) {
 }
 
 fun flatten(root: Node?): Node? {
-    fun helper(root: Node?) : Node? {
-        var cur = root
-
-        while (cur != null) {
-            if (cur.child != null) {
-                var tail = helper(cur.child)
-                var next = cur.next
-                cur.next = cur.child
-                cur.child?.prev = cur
-                tail?.next = next
-                cur.child = null
-                next?.prev = tail
-            }
-
-            if (cur.next == null) {
-               return cur
-            }
-            cur = cur.next
-        }
-
-        return cur
-    }
-
     helper(root)
     return root
 }
 
+fun helper(root: Node?) : Node? {
+    var cur = root
+
+    while (cur != null) {
+        if (cur.child != null) {
+            var tail = helper(cur.child)
+            connect(cur, tail!!)
+        }
+
+        if (cur.next == null) {
+            return cur
+        }
+        cur = cur.next
+    }
+
+    return cur
+}
+
+fun connect(node: Node, tail: Node) {
+    var next = node.next
+    node.next = node.child
+    node.child?.prev = node
+    tail.next = next
+    next?.prev = tail
+    node.child = null
+}
+
 fun main() {
     var node3 = Node(3)
+    var node4 = Node(4)
+    node3.next = node4
 
     var node5 = Node(5)
     node3.child = node5
