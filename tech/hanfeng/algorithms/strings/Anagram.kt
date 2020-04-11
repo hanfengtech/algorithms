915a3c234cab6cd01500815ee1c1b5f7f60ca680
Feng Ping Liu <liufengping@hanfeng.tech>
2020-04-11 15:50:20 -0700

    anagram

---
+++ tech/hanfeng/algorithms/strings/Anagram.kt
@@ -0,0 +1,28 @@
+package tech.hanfeng.algorithms.strings
+
+import java.util.*
+
+fun isAnagram(s : String, t : String) : Boolean {
+    if (s.length != t.length) return false
+
+    val freq = IntArray(26)
+
+    for (i in s.indices) {
+        freq[s[i] - 'a']++
+    }
+
+    for (ch in t) {
+        var i = ch - 'a'
+        freq[i]--
+        if (freq[i] < 0) {
+            return false
+        }
+    }
+
+    return true
+}
+
+fun main() {
+    println(isAnagram("anagram", "nagaram"))
+    println(isAnagram("rat", "cat"))
+}
