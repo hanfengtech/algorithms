39f385d37263c879eb2c2570a569f300ad9d1434
Feng Ping Liu <liufengping@hanfeng.tech>
2020-04-11 14:10:11 -0700

    diameter of binary tree

---
+++ tech/hanfeng/algorithms/tree/DiameterOfBinaryTree.kt
@@ -0,0 +1,30 @@
+package tech.hanfeng.algorithms.tree
+
+import tech.hanfeng.algorithms.tree.utils.TreeNode
+import tech.hanfeng.algorithms.tree.utils.populate123Tree
+import tech.hanfeng.algorithms.tree.utils.preorderPretty
+
+fun diameterOfBinaryTree(root : TreeNode?) : Int {
+    if (root == null) return 0
+    var diameter = Integer.MIN_VALUE
+
+    fun helper(node : TreeNode?) : Int {
+        if (node == null) return 0
+
+        val l = helper(node?.left)
+        val r = helper(node?.right)
+
+        diameter = Math.max(diameter, l + r + 1)
+
+        return Math.max(l, r) + 1
+    }
+
+    helper(root)
+    return diameter 
+}
+
+fun main() {
+    val tree = populate123Tree()
+    preorderPretty(tree)
+    println(diameterOfBinaryTree(tree))
+}
