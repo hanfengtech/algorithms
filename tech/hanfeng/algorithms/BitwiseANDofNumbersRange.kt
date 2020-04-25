7f0fa59ae3fa022166a16a422ade934f767fd8c2
Feng Ping Liu <liufengping@hanfeng.tech>
2020-04-24 20:04:37 -0700

    Bitwise AND of numbers range

---
+++ tech/hanfeng/algorithms/BitwiseANDofNumbersRange.kt
@@ -0,0 +1,18 @@
+package tech.hanfeng.algorithms
+
+fun rangeBitwiseAnd(m: Int, n: Int): Int {
+    var left = m
+    var right = n
+    var count = 0
+    while (left != right) {
+        left = left shr 1
+        right = right shr 1
+        count++
+    }
+    return left shl count
+}
+
+fun main() {
+    println(rangeBitwiseAnd(5,7))
+    println(rangeBitwiseAnd(0,1))
+}
