3c0ff7c7828f99fca8f5c7833dea0807017d112b
Feng Ping Liu <liufengping@hanfeng.tech>
2020-04-25 17:20:25 -0700

    jump game

---
+++ tech/hanfeng/algorithms/array/JumpGame.kt
@@ -0,0 +1,17 @@
+package tech.hanfeng.algorithms.array
+
+fun canJump(A : IntArray) : Boolean {
+    var lastPos = A.size - 1
+    for (i in A.size - 2 downTo 0) {
+        if (i + A[i] >= lastPos) {
+            lastPos = i
+        }
+    }
+
+    return lastPos == 0
+}
+
+fun main() {
+    println(canJump(intArrayOf(2,3,1,1,4)))
+    println(canJump(intArrayOf(3,2,1,0,4)))
+}
