7d8a6b09087d95b611e77ac5854b053eb1d8b586
Feng Ping Liu <liufengping@hanfeng.tech>
2020-05-15 17:10:41 -0700

    save space

--- tech/hanfeng/algorithms/dp/TotalDecodeWays.kt
+++ tech/hanfeng/algorithms/dp/TotalDecodeWays.kt
@@ -3,26 +3,30 @@ package tech.hanfeng.algorithms.dp
 fun numDecodings(s : String) : Int {
     val len = s.length
     val dp = IntArray(len + 1)
-    dp[0] = 1
-    dp[1] = if ('0' == s[0]) 0 else 1
+    var first = 1
+    var second = if ('0' == s[0]) 0 else 1
 
-    for(i in 2 until dp.size) {
+    for (i in 2 until dp.size) {
+        var temp = 0
         val currDigit = s[i - 1] - '0'
         val twoDigits = (s[i - 2] - '0') * 10 + currDigit
 
         if (currDigit >= 1)
-            dp[i] += dp[i - 1]
+            temp += second
 
         if (twoDigits in 10..26) {
-            dp[i] += dp[i-2]
+            temp += first
         }
+
+        first = second
+        second = temp
     }
 
-    return dp[len]
+    return second
 }
 
 fun main() {
-    println(numDecodings("10212"))
+    println(numDecodings("12"))
     println(numDecodings("226"))
     println(numDecodings("220"))
 }
