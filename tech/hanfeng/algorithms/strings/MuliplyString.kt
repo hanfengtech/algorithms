829efd8685b3d079e3fda2a53d1cdb6a8be592b8
Feng Ping Liu <liufengping@hanfeng.tech>
2020-05-15 20:05:13 -0700

    use character function to get numberic

--- tech/hanfeng/algorithms/strings/MuliplyString.kt
+++ tech/hanfeng/algorithms/strings/MuliplyString.kt
@@ -10,7 +10,7 @@ fun multiply(num1 : String, num2 : String) : String {
 
     for (i in m - 1 downTo 0) {
         for (j in n - 1 downTo 0) {
-            val mul = (num1[i] - '0') * (num2[j] - '0')
+            val mul = (Character.getNumericValue(num1[i])) * (num2[j] - '0')
             val p1 = i + j
             val p2 = i + j + 1
             val sum = mul + pos[p2]
