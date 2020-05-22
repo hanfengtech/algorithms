62da86a3f7cf7918dbb0ef08423ba218a5047052
Feng Ping Liu <liufengping@hanfeng.tech>
2020-05-22 14:02:41 -0700

    auto complete

--- tech/hanfeng/algorithms/tree/utils/TrieNode.kt
+++ tech/hanfeng/algorithms/tree/utils/TrieNode.kt
@@ -3,4 +3,23 @@ package tech.hanfeng.algorithms.tree.utils
 class TrieNode {
     var item = ""
     var children = mutableListOf<TrieNode>()
+    var isWord = false
+
+    fun printTrie(root : TrieNode, level : Int = 0) {
+        for (i in root.children.indices) {
+            if (root.children[i] != null) {
+                print("$level  ")
+                for (j in 0 until level) {
+                    print(" ")
+                }
+                println('a' + i)
+                if (root.children[i]!!.isWord) {
+                    println()
+                }
+
+                printTrie(root.children[i]!!, level + 1)
+
+            }
+        }
+    }
 }
