f3ffb571e3c2693241c670ccb23f8c8b5e7e4b0b
Feng Ping Liu <liufengping@hanfeng.tech>
2020-05-29 08:19:49 -0700

    clone graph

--- tech/hanfeng/algorithms/graph/CloneGraph.kt
+++ tech/hanfeng/algorithms/graph/CloneGraph.kt
@@ -1,6 +1,10 @@
 package tech.hanfeng.algorithms.graph
 
-class Node(var `val` : Int, var neighbors : ArrayList<Node> = ArrayList())
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
+
+data class Node(var `val` : Int, var neighbors : ArrayList<Node> = ArrayList())
 
 class CloneGraph {
 
@@ -19,6 +23,34 @@ class CloneGraph {
         for (neighbor in node.neighbors) {
             cloneNode.neighbors.add(cloneGraph(neighbor)!!)
         }
+        return cloneNode
+    }
+
+    fun cloneGraphBFS(node: Node?) : Node? {
+        if (node == null) return node
+
+        var visited = HashMap<Node, Node>()
+
+        var q = LinkedList<Node>()
+
+        val cloneNode = Node(node.`val`)
+
+        visited[node] = cloneNode
+
+        q.offer(node)
+
+        while(q.isNotEmpty()) {
+            var n = q.remove()
+
+            for (neighbor in n.neighbors) {
+                if (!visited.containsKey(neighbor)) {
+                    visited[neighbor] = Node(neighbor.`val`)
+                    q.offer(neighbor)
+                }
+                visited[n]!!.neighbors.add(visited[neighbor]!!)
+            }
+        }
+
         return cloneNode
     }
 }
