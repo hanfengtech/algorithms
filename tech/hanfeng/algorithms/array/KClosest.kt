ceed5194f54691581e768356561e22da7c297ec7
Feng Ping Liu <liufengping@hanfeng.tech>
2020-05-28 20:38:17 -0700

    K closest

---
+++ tech/hanfeng/algorithms/array/KClosest.kt
@@ -0,0 +1,84 @@
+package tech.hanfeng.algorithms.array
+
+import java.util.concurrent.ThreadLocalRandom
+
+class Solution {
+    private lateinit var points : Array<IntArray>
+
+    fun kClosest(points: Array<IntArray>, K: Int): Array<IntArray> {
+        /*
+        points.sortWith(Comparator{ a, b ->
+            when {
+                a[0] * a[0] + a[1] * a[1] > b[0] * b[0] + b[1] * b[1] -> 1
+                a[0] * a[0] + a[1] * a[1] < b[0] * b[0] + b[1] * b[1] -> -1
+                else -> 0
+            }
+        })
+        */
+
+        this.points = points
+        sort(0, points.size - 1, K)
+
+        return points.copyOfRange(0, K)
+    }
+
+    private fun sort(i : Int, j : Int, K : Int) {
+        if (i >= j) {
+            return
+        }
+
+        var k = ThreadLocalRandom.current().nextInt(i,j)
+        swap(i,k)
+
+        val mid = partition(i, j)
+        val leftLength = mid - i + 1
+        if (K < leftLength)
+            sort(i, mid - 1, K)
+        else if (K > leftLength)
+            sort(mid+1, j, K - leftLength)
+    }
+
+    private fun partition(ai : Int, aj : Int) : Int {
+        var i = ai
+        var j = aj
+        val oi = i
+        val pivot = dist(i)
+        i++
+
+        while (true) {
+            while (i < j && dist(i) < pivot)
+                i++
+            while (i < j && dist(j) > pivot)
+                j--
+            if (i >= j) break
+            swap(i, j)
+        }
+        swap(oi, j)
+        return j
+    }
+
+    private fun dist(i : Int) : Int {
+        return points[i][0] * points[i][0] + points[i][1] * points[i][1]
+    }
+
+    private fun swap(i : Int, j : Int) {
+        val t0 = points[i][0]
+        val t1 = points[i][0]
+
+        points[i][0] = points[j][0]
+        points[i][1] = points[j][1]
+        points[j][0] = t0
+        points[j][1] = t1
+    }
+}
+
+fun main() {
+    val s = Solution()
+    val list = s.kClosest(arrayOf(intArrayOf(1,3), intArrayOf(-2,2)), 1)
+    list.forEach {
+        println(it.contentToString())
+    }
+}
+
+
+
