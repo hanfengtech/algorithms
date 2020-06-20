c6f401a9546bd3128dfb464df10dc8d05b5e1646
Feng Ping Liu <liufengping@hanfeng.tech>
2020-06-19 23:55:22 -0700

    slowest key

---
+++ tech/hanfeng/algorithms/array/SlowestKey.kt
@@ -0,0 +1,49 @@
+package tech.hanfeng.algorithms
+
+fun slowestKey(keyTimes: Array<Array<Int>>): Char {
+    // Write your code here
+    /*
+    var max : Int
+    var ch : Int
+
+    if (keyTimes.size == 1) {
+        ch = keyTimes[0][0]
+        max = keyTimes[0][1]
+
+        throw Exception("Illegal Arguments")
+    }
+
+    for (i in 1 until keyTimes.size) {
+        val time = keyTimes[i][1] - keyTimes[i-1][1]
+        if (time > max) {
+            max = time
+            ch = keyTimes[i][0]
+        }
+    }
+
+    return 'a' + ch
+    */
+  return 'a'
+
+}
+
+fun minimumDivisor(arr: IntArray, threshold: Int): Int {
+    var divisor = 1
+    var sum = 0
+
+    do {
+        sum = 0
+        for (i in arr.indices) {
+            sum += Math.ceil(arr[i].toDouble() / divisor).toInt()
+        }
+
+        divisor++
+    } while (sum > threshold)
+
+    arrs divisor - 1
+}
+
+fun main() {
+
+    println(minimumDivisor(intArrayOf(2,4,5),10))
+}
